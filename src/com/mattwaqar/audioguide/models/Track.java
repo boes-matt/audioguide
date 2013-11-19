c60407fe2b57268e78823e8b26bf2ec2ed4f5285
Waqar Malik <wmalik@crimsonresearch.net>
2013-11-18 20:00:03 -0800

    Fixed the merge conflicts

--- src/com/mattwaqar/audioguide/models/Track.java
+++ src/com/mattwaqar/audioguide/models/Track.java
@@ -19,36 +19,12 @@ public class Track extends ParseObject {
 	/*
 	 * A track is an audio track, GPS location, and meta-data.
 	 */
-	public static final String CLASS_NAME = "Track";
-	public static final String TITLE_KEY = "title";
-	public static final String DESCRIPTION_KEY = "description";
-	public static final String AUTHOR_KEY = "author";
-	public static final String AUDIO_RESOURCE_KEY = "audioResource";
-	public static final String AUDIO_PATH_KEY = "audioPath";
-	public static final String LATITUDE_KEY = "latitude";
-	public static final String LONGITUDE_KEY = "longitude";
-	
-	private ParseObject _parseObject;	
-	
-	public Track(ParseObject object) {
-		setParseObject(object);
-	}
 	
 	public static final String TAG = "Track";
-		_parseObject = new ParseObject(CLASS_NAME);
-		setTitle(title);
-		setDescription(description);
-		setAuthor(author);
-		setAudioResource(audioResource);
-		setLocation(latLng);
-	}
 
 	public static final String TITLE = "title";
-		_parseObject = new ParseObject(CLASS_NAME);
-		setTitle(title);
 	public static final String DESCRIPTION = "description";
 	public static final String AUDIO = "audio";	
-		setAudioPath(audioPath);
 	public static final String LATITUDE = "latitude";
 	public static final String LONGITUDE = "longitude";
 	
@@ -71,37 +47,12 @@ public class Track extends ParseObject {
 	public void setDescription(String description) {
 		put(DESCRIPTION, description);
 	}
-
-	public String getAuthor() {
-		return (String) _parseObject.get(AUTHOR_KEY);
-	}
-
-	public void setAuthor(String author) {
-		_parseObject.put(AUTHOR_KEY, author);
-	}
-
-	public void setAudioPath(String audioPath) {
-		_parseObject.put(AUDIO_PATH_KEY, audioPath);
-	}
-
-	public String getAudioPath() {
-		return (String) _parseObject.get(AUDIO_PATH_KEY);
-	}
-
-	public int getAudioResource() {
-		return _parseObject.getInt(AUDIO_RESOURCE_KEY);
-	}
-
-	public void setAudioResource(int audioResource) {
-		_parseObject.put(AUDIO_RESOURCE_KEY, audioResource);
-	}	
 	
 	public ParseFile getAudioFile() {
 		return getParseFile(AUDIO);
 	}
 
 	public void setAudioFile(ParseFile file) {
-		_parseObject.put(LATITUDE_KEY, latLng.latitude);
 		put(AUDIO, file);
 	}
 	
